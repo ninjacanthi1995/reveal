53232747f85100a2a41de894c95ae110c79d21e5
Emilien Dre <emilien.dreyer@gmail.com>
2021-07-26 14:31:14 +0200

    Partial Import feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './component/Home'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import studentList from './reducers/studentList.reducer';
+
+import Home from './component/Home';
+import ImportStudentScreen from './component/ImportStudentScreen';
+import ImportConfigScreen from './component/ImportConfigScreen';
+
+const store = createStore(combineReducers({studentList}));
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        {/* <Route path="/products" component={Products}  /> */}
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/import" component={ImportStudentScreen} />
+          <Route exact path="/import-config" component={ImportConfigScreen} />
+          {/* <Route path="/products" component={Products}  /> */}
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
