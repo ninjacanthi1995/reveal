868ecb92ff9fceaf9d91b87f1fcff8627aba44ed
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2021-07-26 11:36:09 +0200

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './component/Home'
+import TemplateCreator from './component/TemplateCreator'
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/creer-mon-template" component={TemplateCreator}  />
         {/* <Route path="/products" component={Products}  /> */}
       </Switch>
     </Router>
