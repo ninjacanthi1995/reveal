83f1c4bb703e1a92e413da372a64b21ff36dc401
Chau HOANG <hoangminhchaucp54@gmail.com>
2021-07-26 13:29:46 +0200

    Init pdfKit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './component/Home';
+import CreateDiplomas from './component/CreateDiplomas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/create-diplomas" component={CreateDiplomas} />
+      </Switch>
+    </Router>
   );
 }
+
+
 
 export default App;
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,46 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const diplomaModel = require("../models/diplomas");
+// const PDFDocument = require("pdfkit");
+// const fs = require("fs");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-// router.post('/generateDiplome', (req, res) => {
-//   if (req.body.template && req.body.student) {
-//     res.json({ result: true })
-//   } else res.json({ result: false })
-// });
-
-router.post('/orderRide', (req, res) => {
-  let token = req.body.token;
-  let depart = req.body.depart;
-  let destination = req.body.destination;
-
-  if (!token || !depart || !destination) {
-    res.json({ result: false });
+router.post("/create-diploma", async (req, res) => {
+  const searchDiploma = await diplomaModel.findOne({
+    year: req.body.year,
+    curriculum: req.body.curriculum,
+    promo: req.body.promo,
+    schoolId: req.body.schoolId,
+  });
+  if (searchDiploma) {
+    res.json({ result: false, msg: "Diplome est deja existant" });
   } else {
-    // Traitement de la réservation
-    res.json({ result: true, tempsAttente: 10 });
+    const newDiploma = new diplomaModel({
+      name: req.body.name,
+      year: req.body.year,
+      curriculum: req.body.curriculum,
+      promo: req.body.promo,
+      schoolId: req.body.schoolId,
+      studentsId: [],
+    });
+    const savedDiploma = await newDiploma.save();
+
+    // const doc = new PDFDocument();
+    // doc.pipe(fs.createWriteStream("output.pdf"));
+    // doc.text(savedDiploma.year, 100, 100);
+    // doc.end();
+
+    res.json({ result: true, msg: "Diplome cree" });
   }
+});
+
+router.post('/envoyer-diploma', async (req, res) => {
+  const template = await templateModel.findOne({ name: req.body.templateName });
+  
 });
 
 module.exports = router;
