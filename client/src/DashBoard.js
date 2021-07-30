366c0283063552249dbb9bddccd55b1d9b84ac3b
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2021-07-30 14:15:04 +0200

    indentation Dashboard

--- client/src/DashBoard.js
+++ client/src/DashBoard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../src/component/Navbar';
-import { Switch , Checkbox , Button } from 'antd';
-import {Link} from 'react-router-dom';
+import { Switch, Checkbox, Button } from 'antd';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 
@@ -10,36 +10,35 @@ export default function DashBoard() {
 
     function onChange(checked) {
         console.log(`switch to ${checked}`);
-}
+    }
 
 
 
 
-        return (
+    return (
         <>
-        <Navbar/>
-        <div>
-        
-        <div className="dashboard-page" style={{backgroundImage: "url('/dashboard2.png')" , opacity: "75%"}}>
-         <div>  
-         <h1 className="dashboard-h1" >Dashboard</h1>
-        <div style={{marginTop : 40}}>
-            <div className="dashboard" style={{marginTop : 40 }} >
-                <Link to="/creer-mon-template" style={{marginLeft : 200 ,fontWeight: "bold" , fontSize:'1.2em' }}>1/ Créer votre premier diplôme</Link>
-                <Switch defaultChecked onChange={onChange} style={{marginRight : 200 }}/>
+            <Navbar />
+            <div>
+                <div className="dashboard-page" style={{ backgroundImage: "url('/dashboard2.png')", opacity: "75%" }}>
+                    <div>
+                        <h1 className="dashboard-h1" >Dashboard</h1>
+                        <div style={{ marginTop: 40 }}>
+                            <div className="dashboard" style={{ marginTop: 40 }} >
+                                <Link to="/creer-mon-template" style={{ marginLeft: 200, fontWeight: "bold", fontSize: '1.2em' }}>1/ Créer votre premier diplôme</Link>
+                                <Switch defaultChecked onChange={onChange} style={{ marginRight: 200 }} />
+                            </div>
+                            <div className="dashboard" style={{ marginTop: 40 }}>
+                                <Link to="/create-batch" style={{ marginLeft: 200, fontWeight: "bold", fontSize: '1.2em' }}>2/ Importez vos diplômes</Link>
+                                <Switch defaultChecked onChange={onChange} style={{ marginRight: 200 }} />
+                            </div>
+                            <div className="dashboard" style={{ marginTop: 40 }}>
+                                <Link to="/#" style={{ marginLeft: 200, marginRight: 30, fontWeight: "bold", fontSize: '1.2em' }}>3/ Envoyer les diplômes aux élèves 🎉</Link>
+                                <Switch defaultChecked onChange={onChange} style={{ marginRight: 200 }} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="dashboard" style={{marginTop : 40}}>
-            <Link to="/create-batch" style={{marginLeft : 200, fontWeight: "bold", fontSize:'1.2em' }}>2/ Importez vos diplômes</Link>
-                <Switch defaultChecked onChange={onChange} style={{marginRight : 200 }}/>
-            </div>
-            <div className="dashboard" style={{marginTop : 40}}>
-            <Link to="/#" style={{marginLeft : 200, marginRight:30 , fontWeight: "bold", fontSize:'1.2em'}}>3/ Envoyer les diplômes aux élèves 🎉</Link>
-                <Switch defaultChecked onChange={onChange} style={{marginRight : 200 }}/>
-            </div>
-        </div>
-        </div>
-        </div>
-        </div> 
         </>
 
 
