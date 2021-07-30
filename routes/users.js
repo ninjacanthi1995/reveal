69073a4180f7ab00a94f617af4bad2c15cef309e
Dorian Gentine <dorian.gentine@mail.novancia.fr>
2021-07-30 10:52:04 +0200

    complete connexion

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const SchoolModel = require("../models/schools");
 
 //fred : ajout du require users  dans une const nommee UserModel pour call la db
 const UserModel = require("../models/users");
@@ -9,8 +10,40 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.post("/sign-up", async (req, res) => {
+  console.log(`req.body`, req.body)
+  const user = {...req.body}
+  if(user.firstname === "" &&
+    user.email === "" &&
+    user.password === "" &&
+    user.admin === "" &&
+    user.school_name === ""
+  ) {
+    res.json({result: false, error: "Il nous manque des infos"})
+  }
+  
+  const school = await SchoolModel.findOne({ name: user.school_name })
+  if(!school){
+    res.json({result: false, error: "Nous n'avons pas trouver votre école"})
+  } else {
+    const check = await UserModel.find({ email: user.email })
+    if(check.length !== 0){
+      res.json({result: false, error: "Cet email est déjà pris !"})
+    }else{
+      const newUser = new UserModel({...user})
+      newUser.school_id = school.id
+      const userSaved = newUser.save()
+      if(userSaved){
+        res.json({result: true, message: "Utilisateur créé"})
+      }else{
+        res.json({result:false, error: "L'utilisateur n'a pas pus être créé"})
+      }
+    }
+  }
+})
+
 router.post("/sign-in", async (req, res) => {
-  console.log(req.body);
+  console.log(`req.body`, req.body)
   var result = false;
   var user = null;
   var error = [];
